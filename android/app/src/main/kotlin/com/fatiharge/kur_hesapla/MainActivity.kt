7d7cc9e0fe2023d6de5bbeb49dac00af506a5cd7
fatihcetin <fatihcetin@tamfinans.com.tr>
2024-12-16 09:48:35 +0300

    feat: init code implemented

---
+++ android/app/src/main/kotlin/com/fatiharge/kur_hesapla/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.fatiharge.kur_hesapla
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ script/api.sh
@@ -0,0 +1,4 @@
+openapi-generator-cli generate \
+    -i https://kur-hesapla-api.fatiharge.com/q/openapi \
+    -g dart-dio \
+    -o ../package/kur_hesapla_api_client
